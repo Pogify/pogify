b19573734f3edb3f43f0c8053702edca6d9dee6d
KentoNishi <kento24gs@outlook.com>
2020-08-11 16:57:17 -0700

    Added youtube search api

---
+++ yt.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+from youtube_search import YoutubeSearch
+
+
+@app.get("/")
+def search(q: str):
+    results = YoutubeSearch(q, max_results=1).to_dict()
+    if len(results) == 0:
+        return ""
+    return results[0]["url_suffix"]
