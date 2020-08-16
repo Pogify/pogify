1cd098a75ede915e60b3b892630966e2e59cff32
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2020-08-16 11:31:52 +1000

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -21,7 +21,7 @@ class Home extends React.Component {
     render() {
         return (
         <div style={{height: '100vh', width: '100vw', display: "flex", justifyContent: "center", alignItems: "center", backgroundImage: "linear-gradient(to right, #7f53ac 0, #657ced 100%)"}}>
-            <div style={{background: 'white', color: "black", mixBlendMode: "screen", padding: '2rem', boxShadow: '10px 5px 5px black'}}>
+            <div style={{background: 'white', color: "black", mixBlendMode: "screen", padding: '2rem', borderRadius: '12.5px', boxShadow: '10px 5px 5px black'}}>
                 <h1 style={{textAlign: 'center', fontSize: '5em', letterSpacing: '7px', margin: 0, fontWeight: 'bold'}}>POGIFY</h1>
                 <div style={{display: 'flex', justifyContent: 'space-between'}}>
                     <Link to="/session/">
