bb92258a561749d7d1136318ca5b758469a8fb7d
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-08-17 22:41:23 -0400

    create uses function

--- src/Create.jsx
+++ src/Create.jsx
@@ -7,50 +7,53 @@ export class Create extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      hosting: [],
+      activeSession: "",
     };
     this.create = this.create.bind(this);
   }
 
-  create() {
-    axios
-      .post("/create", this.state.session, {
-        headers: {
-          "Content-Type": "text/plain",
-        },
-      })
-      .then((res) => {
-        window.location.href = `/session/${res.data}`;
-        console.log(res.data);
-      })
-      .catch(console.error);
+  async create() {
+    this.backoff = 1;
+    try {
+      let { data } = await axios.post(
+        "https://us-central1-pogify-database.cloudfunctions.net/startSession"
+      );
+
+      window.localStorage.setItem("token", data.token);
+      window.localStorage.setItem(
+        "expiresAt",
+        data.expiresIn * 1000 + Date.now()
+      );
+      window.localStorage.setItem("session", data.session);
+      this.props.history.push("/session/" + data.session);
+    } catch (e) {}
   }
 
   componentDidMount() {
-    axios.get("/getSessions").then((res) => {
+    if (
+      window.localStorage.getItem("expiresAt") <
+      Date.now() + 50 * 60 * 1000
+    ) {
       this.setState({
-        hosting: res.data,
+        activeSession: window.localStorage.getItem("session"),
       });
-    });
+    }
   }
 
   render() {
     return (
       <Layout>
-        {Boolean(this.state.hosting.length) && (
-          <div>
-            Your Current Listening Sessions:
-            <ul>
-              {this.state.hosting.map((item) => {
-                return (
-                  <li key={item}>
-                    <Link to={`/session/${item}`} style={{ color: "unset" }}>
-                      {item}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
+        {Boolean(this.state.activeSession) && (
+          <div style={{ textAlign: "center", margin: 5 }}>
+            Your Active Session:
+            <div>
+              <Link
+                to={`/session/${this.state.activeSession}`}
+                style={{ color: "unset" }}
+              >
+                {this.state.activeSession}
+              </Link>
+            </div>
           </div>
         )}
         <button onClick={this.create}>Create New Listening Session</button>
