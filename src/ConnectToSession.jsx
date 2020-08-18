514937735b961bd5efbab2d10c8c9daf495e3fe1
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-08-17 23:01:42 -0400

    styling

--- src/ConnectToSession.jsx
+++ src/ConnectToSession.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import Layout from "./Layout";
+import styled from "styled-components";
+
+const CodeInput = styled.input`
+  width: calc(100% - 10px);
+  padding: 5px;
+  border-radius: 12px;
+  border-style: solid;
+  border-width: 0.5px;
+  border-color: rgb(44, 58, 58);
+  text-align: center;
+  transition: box-shadow 0.3s;
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24);
+  }
+`;
 
 export const ConnectToSession = () => {
   const [code, setCode] = React.useState("");
@@ -12,18 +29,14 @@ export const ConnectToSession = () => {
 
   return (
     <Layout>
-      <h2>Enter Session Code</h2>
-      <form onSubmit={submit}>
+      <h2 style={{ marginTop: 0 }}>Enter Session Code</h2>
+      <form onSubmit={submit} style={{ display: "inline" }}>
         <div>
-          <input
+          <CodeInput
             type="text"
             placeholder="Code"
             onChange={(e) => setCode(e.target.value)}
-            style={{
-              width: "calc(100% - 10px)",
-              padding: "5px",
-              textAlign: "center",
-            }}
+            style={{}}
           />
         </div>
         <div>
@@ -31,11 +44,11 @@ export const ConnectToSession = () => {
             type="submit"
             style={{
               width: "100%",
-              margin: 0,
+              margin: "auto",
               marginTop: "1rem",
             }}
           >
-            Go to session
+            Go
           </button>
         </div>
       </form>
