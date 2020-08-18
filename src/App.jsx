600a52e29db80e47ecbc4ed353cff7c9d631bc68
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-08-17 22:43:30 -0400

    remove unused context provider

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,18 @@ import { ConnectToSession } from "./ConnectToSession";
 import Home from "./Home";
 import "./App.css";
 
-const UserContext = React.createContext({});
-export const io = window.io;
-
 function App() {
   return (
-    <UserContext.Provider value={{}}>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/create" exact component={Create} />
-          <Route path="/auth" exact component={AuthRedirect} />
-          <Route path="/session/:id" component={Room} />
-          <Route path="/session" component={ConnectToSession} exact />
-          <Route path="/">404</Route>
-        </Switch>
-      </BrowserRouter>
-    </UserContext.Provider>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/create" exact component={Create} />
+        <Route path="/auth" exact component={AuthRedirect} />
+        <Route path="/session/:id" component={Room} />
+        <Route path="/session" component={ConnectToSession} exact />
+        <Route path="/">404</Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
