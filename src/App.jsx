c127752d90576b981dc37886324f1ebe57e4e920
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-08-20 19:56:21 -0400

    better 404 message

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import { AuthRedirect } from "./AuthRedirect";
 import { Room } from "./Room";
 import { Create } from "./Create";
 import { ConnectToSession } from "./ConnectToSession";
 import Home from "./Home";
 import "./App.css";
+import Layout from "./Layout";
 
 function App() {
   return (
@@ -16,7 +17,17 @@ function App() {
         <Route path="/auth" exact component={AuthRedirect} />
         <Route path="/session/:id" component={Room} />
         <Route path="/session" component={ConnectToSession} exact />
-        <Route path="/">404</Route>
+        <Route path="/">
+          <Layout>
+            <div style={{ textAlign: "center" }}>
+              <h1 style={{ margin: 0 }}>
+                <div>404</div>
+              </h1>
+              <div>What you were looking for isn't here</div>
+              <Link to="/">Go home</Link>
+            </div>
+          </Layout>
+        </Route>
       </Switch>
     </BrowserRouter>
   );
