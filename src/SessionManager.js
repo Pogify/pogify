5e452a11bcee71f8cb9e5709a906986f4556f6ec
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-08-20 15:10:50 -0400

    use env var for firebaseconfig

--- src/SessionManager.js
+++ src/SessionManager.js
@@ -3,7 +3,12 @@ import axios from "axios";
 import * as firebase from "firebase/app";
 import "firebase/auth";
 
-var firebaseConfig = require("../firebaseConfig.json");
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
 
 firebase.initializeApp(firebaseConfig);
 const FBAuth = firebase.auth();
