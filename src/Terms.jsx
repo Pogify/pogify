07de4e064968a40132ebbed2f131a8414d4141db
emtee14 <48140121+emtee14@users.noreply.github.com>
2020-08-21 02:37:53 +0100

    Updated ToS and Privacy Policy

--- src/Terms.jsx
+++ src/Terms.jsx
@@ -5,13 +5,19 @@ export const Terms = () => {
   return (
     <Layout>
         <div>
-        <h1 id="terms-and-conditions">Terms and Conditions</h1>
-        <p>Below are the Terms and Conditions for use of pogify.net. Please read these carefully. By accessing the content of pogify.net you agree to the terms and conditions set out herein and also accept our Privacy Policy. If you do <strong>not</strong> agree to any of the terms and conditions you should <strong>not</strong> continue to use pogify.net and leave immediately.</p>
-        <p>You agree that you shall <strong>not</strong> use pogify.net for any illegal purposes and that you will respect all applicable laws and regulations.</p>
-        <p>You agree <strong>not</strong> to use pogify.net in a way that may impair the performance, corrupt or manipulate the content or information available on pogify.net or reduce the overall functionality of pogify.net.</p>
-        <p>You agree <strong>not</strong> to compromise the security of pogify.net or attempt to gain access to secured areas of pogify.net or attempt to access any sensitive information you may believe exist on pogify.net or server where it is hosted.</p>
-        <p>You agree to be fully responsible for any claim, expense, losses, liability, costs including legal fees incurred by us arising from any infringement of the terms and conditions in this agreement and to which you will have agreed if you continue to use pogify.net.</p>
-        <p>The work on pogify.net and the images, logos, text and other such information is the property of pogify.net ( unless otherwise stated ).</p>
+          <h1>Terms and Conditions</h1>
+          <h2>Pogify.net</h2>
+          <p>Below are the Terms and Conditions for use of <a href="http://pogify.net">pogify.net</a>. Please read these carefully.<br/>By accessing the content of <a href="http://pogify.net">pogify.net</a> you agree to the terms and conditions set out herein and also accept our Privacy Policy. If you do not agree to any of the terms and conditions you should not continue to use <a href="http://pogify.net">pogify.net</a> and leave immediately.</p>
+          <p>You agree that you shall not use <a href="http://pogify.net">pogify.net</a> for any illegal purposes and that you will respect all applicable laws and regulations.</p>
+          <p>You agree not to use <a href="http://pogify.net">pogify.net</a> in a way that may impair the performance, corrupt or manipulate the content or information available on <a href="http://pogify.net">pogify.net</a> or reduce the overall functionality of <a href="http://pogify.net">pogify.net</a>.</p>
+          <p>You agree not to compromise the security of <a href="http://pogify.net">pogify.net</a> or attempt to gain access to secured areas of <a href="http://pogify.net">pogify.net</a> or attempt to access any sensitive information you may believe exist on <a href="http://pogify.net">pogify.net</a> or server where it is hosted.</p>
+          <p>You agree to be fully responsible for any claim, expense, losses, liability, costs including legal fees incurred by us arising from any infringement of the terms and conditions in this agreement and to which you will have agreed if you continue to use <a href="http://pogify.net">pogify.net</a>.</p>
+          <p>The work on <a href="http://pogify.net">pogify.net</a> and the images, logos, text and other such information is the property of <a href="http://pogify.net">pogify.net</a> ( unless otherwise stated ).</p>
+          <h2>Spotify</h2>
+          <p>You agree that you will not modify or create derivative works based on the Spotify Platform, Spotify Service or Spotify Content</p>
+          <p>You agree that you will not decompile, reverse-engineer, disassemble, and otherwise reducing the Spotify Platform, Spotify Service, and Spotify Content to source code or other human-perceivable form, to the full extent allowed by law</p>
+          <p><a href="http://Pogify.net">Pogify.net</a> takes full liabilty of it’s products and disclaims any liabilty from Spotify</p>
+          <p>Spotify is a third party beneficiary of <a href="http://Pogify.net">Pogify.net</a> end user license agreement and privacy policy and is entitled to directly enforce them</p>
         </div>
     </Layout>
   );
