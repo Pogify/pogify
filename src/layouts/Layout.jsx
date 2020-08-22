3165af64448f2e440555a5429e8c024f50b24797
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2020-08-22 18:41:31 +1000

    Implement dark mode

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,64 +1,85 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Layout.css";
-export const Layout = (props) => {
-  return (
-    <div
-      style={{
-        height: "100vh",
-        width: "100vw",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+export default class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      darkMode: 'false'
+    }
+    this.toggleDarkMode = this.toggleDarkMode.bind(this);
+  }
+  componentDidMount() {
+    const storedDarkMode = localStorage.getItem('darkMode') === 'true';
+    this.setState({darkMode : storedDarkMode})
+  }
+
+  toggleDarkMode() {
+    const newDarkMode = !(this.state.darkMode);
+    this.setState({darkMode : newDarkMode})
+    localStorage.setItem('darkMode', newDarkMode);
+  }
+
+  render() {
+    let darkMode = this.state.darkMode;
+    return (
       <div
         style={{
-          background: "white",
-          color: "#2C3A3A",
-          padding: "2rem",
-          borderRadius: "12.5px",
-          boxShadow: "0px 3px 15px rgba(0,0,0,0.2)",
-          maxWidth: "90%",
-          maxHeight: "80%",
-          overflow: "auto",
+          height: "100vh",
+          width: "100vw",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        {props.children}
-      </div>
-      <p
-        style={{
-          position: "absolute",
-          bottom: "0",
-          left: "50%",
-          transform: "translateX(-50%)",
-          color: "white",
-        }}
-      >
-        &nbsp;
+        <div
+          style={{
+            transition: 'background 0.5s, color 0.5s',
+            background: darkMode ? '#222222' : 'white',
+            color: darkMode ? 'white' : '#222222',
+            padding: "2rem",
+            borderRadius: "12.5px",
+            boxShadow: "0px 3px 15px rgba(0,0,0,0.2)",
+            maxWidth: "90%",
+            maxHeight: "80%",
+            overflow: "auto",
+          }}
+        >
+          {this.props.children}
+        </div>
+
+        <p
+          style={{
+            position: "absolute",
+            bottom: "0",
+            left: "50%",
+            transform: "translateX(-50%)",
+            color: "white",
+            textAlign: "center"
+          }}
+        >
+          &nbsp;
         ©{" "}
-        <a href="https://www.pogify.net/" style={{ color: "white" }}>
-          Pogify
+          <a href="https://www.pogify.net/" style={{ color: "white" }}>
+            Pogify
         </a>{" "}
-        2020 |{" "}
-        <Link to="/tos" style={{ color: "white" }}>
-          Terms of Service
+        |{" "}
+          <Link to="/tos" style={{ color: "white" }}>
+            Terms of Service
         </Link>{" "}
         |{" "}
-        <Link to="/privacy" style={{ color: "white" }}>
-          Privacy Policy
-        </Link>
-        <br/>
-        <center>
-          Powered by&nbsp;
-          <a href="https://spotify.com">
-            <img width="60px" style={{verticalAlign: "middle"}} src="/spotify-logo.png"/>
-          </a>
-        </center>
+          <Link to="/privacy" style={{ color: "white" }}>
+            Privacy Policy
+        </Link>{" "}
+        |{" "}
+          <a href="#" onClick={this.toggleDarkMode} style={{ color: "white" }}>Switch to {darkMode ? 'Light' : 'Dark'} Mode!</a>
+          <br />
+          <a href="https://www.spotify.com" style={{ color: "white" }}>Spotify</a> is copyright Spotify AB and is not affiliated with Pogify.
       </p>
-      
-    </div>    
-  );
+
+      </div>
+    );
+  }
 };
 
 // Layout.propTypes = {
