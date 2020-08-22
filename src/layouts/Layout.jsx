1d56b98b3283e1197da2c845d08f1c2d67a0eb2b
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2020-08-22 19:54:49 +1000

    Change inline styling to css classes

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -12,74 +12,39 @@ export default class Layout extends React.Component {
   }
   componentDidMount() {
     const storedDarkMode = localStorage.getItem('darkMode') === 'true';
-    this.setState({darkMode : storedDarkMode})
+    this.setState({ darkMode: storedDarkMode })
   }
 
   toggleDarkMode() {
     const newDarkMode = !(this.state.darkMode);
-    this.setState({darkMode : newDarkMode})
+    this.setState({ darkMode: newDarkMode })
     localStorage.setItem('darkMode', newDarkMode);
   }
 
   render() {
     let darkMode = this.state.darkMode;
+
+    let contentClass = 'content';
+    if (darkMode) {
+      contentClass += ' darkContent';
+    }
+
     return (
-      <div
-        style={{
-          height: "100vh",
-          width: "100vw",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <div
-          style={{
-            transition: 'background 0.5s, color 0.5s',
-            background: darkMode ? '#222222' : 'white',
-            color: darkMode ? 'white' : '#222222',
-            padding: "2rem",
-            borderRadius: "12.5px",
-            boxShadow: "0px 3px 15px rgba(0,0,0,0.2)",
-            maxWidth: "90%",
-            maxHeight: "80%",
-            overflow: "auto",
-          }}
-          className={darkMode ? "darkMode" : ""}
-        >
+      <div className="layout">
+        <div className={contentClass}>
           {this.props.children}
         </div>
-
-        <p
-          style={{
-            position: "absolute",
-            bottom: "0",
-            left: "50%",
-            transform: "translateX(-50%)",
-            color: "white",
-            textAlign: "center"
-          }}
-        >
-          &nbsp;
-        ©{" "}
-          <a href="https://www.pogify.net/" style={{ color: "white" }}>
-            Pogify
-        </a>{" "}
-        |{" "}
-          <Link to="/tos" style={{ color: "white" }}>
-            Terms of Service
-        </Link>{" "}
-        |{" "}
-          <Link to="/privacy" style={{ color: "white" }}>
-            Privacy Policy
-        </Link>{" "}
-        |{" "}
-          <a href="#" onClick={this.toggleDarkMode} style={{ color: "white" }}>Switch to {darkMode ? 'Light' : 'Dark'} Mode!</a>
+        <footer className="footer">
+          ©{" "}
+          <a href="https://www.pogify.net/">Pogify</a>{" "}|{" "}
+          <Link to="/tos">Terms of Service</Link>{" "}|{" "}
+          <Link to="/privacy">Privacy Policy</Link>{" "}|{" "}
+          <button onClick={this.toggleDarkMode} className={"mode-toggle"}>Switch to {darkMode ? 'Light' : 'Dark'} Mode!</button>
           <br />
-          <a href="https://www.spotify.com" style={{ color: "white" }}>Spotify</a> is copyright Spotify AB and is not affiliated with Pogify.
-      </p>
+          <a href="https://www.spotify.com">Spotify</a> is copyright Spotify AB and is not affiliated with Pogify.
+        </footer>
 
-      </div>
+      </div >
     );
   }
 };
