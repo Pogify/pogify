69f190b288f783fd04196e636d2f7a90557f5821
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2020-08-22 21:11:26 +1000

    Rewrite Terms of Use

--- src/routes/Terms.jsx
+++ src/routes/Terms.jsx
@@ -5,54 +5,52 @@ export const Terms = () => {
   return (
     <Layout>
       <div>
-        <h1 id="terms-and-conditions">Terms and Conditions</h1>
+        <h1 id="terms-of-use">Pogify Terms of Use</h1>
         <p>
-          Below are the Terms and Conditions for use of pogify.net. Please read
-          these carefully. By accessing the content of pogify.net you agree to
-          the terms and conditions set out herein and also accept our Privacy
-          Policy. If you do <strong>not</strong> agree to any of the terms and
-          conditions you should <strong>not</strong> continue to use pogify.net
-          and leave immediately.
+          The pogify.net website located at http://www.pogify.net is governed by the GNU Affero General Public License v3. 
+          As such, Pogify software, to the extent permitted by applicable law, provides no warranty of any kind and is provided on an "AS IS" basis.
+          By using the Pogify service or any software developed under the Pogify brand, you agree to be bound to the GNU Affero General Public License v3 and the terms outlined by such.
+          Certain software developed by Pogify contributors may be subject to licensing or terms differing from those within the GNU Affero General Public License v3 and this document. 
+          Should that be the case, the applicable license and other legal information will be explicitly written in an area reasonably prominent to those using the software who will be bound by the document.
+          pogify.net, and any software developed under the Pogify brand by the Pogify team, will henceforth be referred to as "Pogify's services and software".
         </p>
         <p>
-          You agree that you shall <strong>not</strong> use pogify.net for any
-          illegal purposes and that you will respect all applicable laws and
-          regulations.
+          Pogify refuses to deliver its services and software to the following users:
+          <ol>
+            <li>A person under the age of 13</li>
+            <li>A person legally barred or otherwise restricted from receiving and rendering services under the laws of the United States or otherwise applicable jurisdiction</li>
+            <li>A person suspended from using either Pogify's or Spotify's services and software</li>
+            <li>A person in posession of a "cracked" or stolen Spotify Premium account.</li>
+          </ol>
+          We may, in our sole discretion, refuse any user access to any of Pogify's services and software.
         </p>
         <p>
-          You agree <strong>not</strong> to use pogify.net in a way that may
-          impair the performance, corrupt or manipulate the content or
-          information available on pogify.net or reduce the overall
-          functionality of pogify.net.
+          By using the pogify.net service or any software developed by Pogify, you agree that you shall not use such for any illegal purposes and that you will respect all applicable laws and regulations.
+          You also agree to not use our services and software in a way that impairs or otherwise degrades the performance or delivery of our services and software.
+          Although the project is open-sourced, you agree not to modify the content hosted on the pogify.net website.
+          You also agree to not use Pogify's services and software in any way that may be deemed, in Pogify's sole discretion, as abuse including: maintaining numerous unnecessary connections, stress-testing our services without written consent from Pogify, connecting with bots, or circumventing any security system developed by Pogify.
+          Should you be using our services and software in a manner that either violates any form of law or impairs performance as outlined above, your access to Pogify's content will be denied.
         </p>
         <p>
-          You agree <strong>not</strong> to compromise the security of
-          pogify.net or attempt to gain access to secured areas of pogify.net or
-          attempt to access any sensitive information you may believe exist on
-          pogify.net or server where it is hosted.
+          You also agree that you will not attempt to compromise the security of Pogify's services and software.
+          You agree that you will not attempt to directly modify the service or content present on the pogify.net website.
+          You agree to not attempt to gain access to secured areas present within Pogify's services and software.
+          Should you locate a security vulnerability, you agree to not disclose the vulnerability within a public setting and instead agree to disclose it to us at support@pogify.net or on any other platform of official Pogify contact.
         </p>
         <p>
-          You agree to be fully responsible for any claim, expense, losses,
-          liability, costs including legal fees incurred by us arising from any
-          infringement of the terms and conditions in this agreement and to
-          which you will have agreed if you continue to use pogify.net.
+          Verbatim from the GNU Affero General Public License v3, "IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MODIFIES AND/OR CONVEYS THE PROGRAM AS PERMITTED ABOVE, BE LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER PROGRAMS), EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES."
         </p>
         <p>
-          The work on pogify.net and the images, logos, text and other such
-          information is the property of pogify.net ( unless otherwise stated ).
+          The work present within Pogify's services and software, unless otherwise governed by the GNU Affero General Public License v3 or otherwise explicitly stated as belonging to the public domain, is the property of Pogify.
         </p>
         <h2>Spotify</h2>
         <p>
-          You agree that you will not modify or create derivative works based on the Spotify Platform, Spotify Service or Spotify Content.
-        </p>
-        <p>
-          You agree that you will not decompile, reverse-engineer, disassemble, and otherwise reducing the Spotify Platform, Spotify Service, and Spotify Content to source code or other human-perceivable form, to the full extent allowed by law.
-        </p>
-        <p>
-          <a href="http://Pogify.net">Pogify.net</a> takes full liabilty of it’s products and disclaims any liabilty from Spotify.
-        </p>
-        <p>
-          Spotify is a third party beneficiary of <a href="http://Pogify.net">Pogify.net</a> end user license agreement and privacy policy and is entitled to directly enforce them.
+          We, Pogify, disclaim all liability from Spotify and will not make any warranties or representations on behalf of Spotify. We disclaim all implied warranties pertaining to the Spotify Platform, Spotify Service and Spotify Content, and instead provide no warranty, as permissible by law and as outlined by the GNU Affero General Public License v3.
+          You acknowledge that Spotify will not provide any warranty in any regard to Pogify's services and software, and to the extent permissible by law, neither will Pogify.
+          You agree not to use the Pogify platform to modify or create derivative works based on any content owned by Spotify including the Spotify Platform, Spotify Service or Spotify content.
+          You agree that you do not have the same freedoms to decompile, reverse-engineer, disassemble, and otherwise reduce the Spotify Platform, Spotify Service, and Spotify Content to source code or other human-perceivable form, to the full extent allowed by law, as you do with Pogify's services and software.
+          Pogify disclaims any and all liabilty from Spotify.
+          Spotify is a third party beneficiary of Pogify's software and services and as such, Spotify is entitled to directly enforce the terms outlined within this document.
         </p>
       </div>
     </Layout>
