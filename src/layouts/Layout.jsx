8b4e73c1db4c996cdc50907e1138db115cf33b9b
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2020-08-24 21:45:33 +1000

    Revert dark mode button

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../styles/Layout.css";
 import {observer} from "mobx-react"
 import { useStores } from "../hooks/useStores";
@@ -18,12 +16,12 @@ const Layout = ({children}) => {
 
   return (
       <div className="layout">
-        <button onClick={toggleTheme} className={"mode-toggle"}><FontAwesomeIcon icon={darkMode ? faSun : faMoon} size="3x" style={{ color: darkMode ? '#ffffff' : '#222222' }}/></button>
         <div className={contentClass}>{children}</div>
         <footer className="footer">
           © <a href="https://www.pogify.net/">Pogify</a> |&nbsp;
           <Link to="/tou">Terms of Use</Link> |&nbsp;
-          <Link to="/privacy">Privacy Policy</Link>
+          <Link to="/privacy">Privacy Policy</Link> |&nbsp;
+          <button onClick={toggleTheme} className={"mode-toggle"}>{darkMode ? "Switch to light mode!" : "Switch to dark mode!"}</button>
           <br/>
           <a href="https://www.spotify.com">Spotify</a> is copyright Spotify AB
           and is not affiliated with Pogify.
