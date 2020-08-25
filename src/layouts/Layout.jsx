8b9438b91e5da8721b644514fa01d8d9bbfb6dc4
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2020-08-25 18:07:00 +1000

    Use new footer

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -6,7 +6,7 @@ import { useStores } from "../hooks/useStores";
 
 
 const Layout = ({children}) => {
-  let {theme, toggleTheme} = useStores().themeStore
+  let {theme/*, toggleTheme*/} = useStores().themeStore
   
   let contentClass = "content";
   const darkMode = theme === "dark"
@@ -18,13 +18,12 @@ const Layout = ({children}) => {
       <div className="layout">
         <div className={contentClass}>{children}</div>
         <footer className="footer">
-          © <a href="https://www.pogify.net/">Pogify</a> |&nbsp;
-          <Link to="/tou">Terms of Use</Link> |&nbsp;
-          <Link to="/privacy">Privacy Policy</Link> |&nbsp;
-          <button onClick={toggleTheme} className={"mode-toggle"}>{darkMode ? "Switch to light mode!" : "Switch to dark mode!"}</button>
-          <br/>
-          <a href="https://www.spotify.com">Spotify</a> is copyright Spotify AB
-          and is not affiliated with Pogify.
+          <div className="footer-links">
+            <Link to="/tou">Terms of Use</Link>
+            <Link to="/privacy">Privacy Policy</Link>
+            <a href="https://github.com/Pogify/pogify">GitHub</a>
+          </div>
+          <p><a href="https://www.spotify.com">Spotify</a> is copyright Spotify AB and is not affiliated with Pogify</p>
         </footer>
       </div>
 
