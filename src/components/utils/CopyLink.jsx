a82eb737a555bcb14ee229b44980f77b533d7388
Arno <arno.du@orange.fr>
2020-09-01 12:41:06 +0200

    Select link when clicking on it

--- src/components/utils/CopyLink.jsx
+++ src/components/utils/CopyLink.jsx
@@ -2,14 +2,32 @@ import React, { useState } from 'react';
 import NewTabLink from './NewTabLink';
 
 import styles from "./CopyLink.module.css";
+import { useRef } from 'react';
+
+function selectElementText(el) {
+    var doc = window.document, sel, range;
+    if (window.getSelection && doc.createRange) {
+        sel = window.getSelection();
+        range = doc.createRange();
+        range.selectNodeContents(el);
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (doc.body.createTextRange) {
+        range = doc.body.createTextRange();
+        range.moveToElementText(el);
+        range.select();
+    }
+}
 
 export default function CopyLink({ children, className, title, ...props }) {
     const linkTitle = typeof title !== "undefined" ? title : "Click to copy and share"
     const [tooltipState, setTooltipState] = useState(false);
     const [tooltipText, setTooltipText] = useState(linkTitle);
+    const linkRef = useRef(null);
     const copyLink = (evt) => {
         evt.preventDefault();
-        if (navigator.clipboard.writeText) {
+        selectElementText(linkRef.current)
+        if ("clipboard" in navigator) {
             navigator.clipboard.writeText(evt.target.href);
             setTooltipText("Copied!")
         }
@@ -26,7 +44,7 @@ export default function CopyLink({ children, className, title, ...props }) {
     return (
         <div className={styles.wrapper}>
             <span className={`${styles.tooltip} ${tooltipState ? styles.shown : styles.hidden}`} aria-hidden>{tooltipText}</span>
-            <NewTabLink title={linkTitle} onClick={copyLink} onMouseEnter={() => setTooltipState(true)} onMouseLeave={handleMouseLeave} className={`${styles.link} ${className}`} {...props}>
+            <NewTabLink title={linkTitle} onClick={copyLink} ref={linkRef} onMouseEnter={() => setTooltipState(true)} onMouseLeave={handleMouseLeave} className={`${styles.link} ${className}`} {...props}>
                 {children}
             </NewTabLink>
         </div>
