5f140505314fdb021467471bb6bfbeb26277d7f8
KentoNishi <kento24gs@outlook.com>
2020-09-01 19:22:00 -0700

    Patch for "copy link" function in edge legacy

--- src/components/utils/CopyLink.jsx
+++ src/components/utils/CopyLink.jsx
@@ -4,33 +4,18 @@ import NewTabLink from './NewTabLink';
 import styles from "./CopyLink.module.css";
 import { useRef } from 'react';
 
-function selectElementText(el) {
-    var doc = window.document, sel, range;
-    if (window.getSelection && doc.createRange) {
-        sel = window.getSelection();
-        range = doc.createRange();
-        range.selectNodeContents(el);
-        sel.removeAllRanges();
-        sel.addRange(range);
-    } else if (doc.body.createTextRange) {
-        range = doc.body.createTextRange();
-        range.moveToElementText(el);
-        range.select();
-    }
-}
+
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+
 
 export default function CopyLink({ children, className, title, ...props }) {
     const linkTitle = typeof title !== "undefined" ? title : "Click to copy and share"
     const [tooltipState, setTooltipState] = useState(false);
     const [tooltipText, setTooltipText] = useState(linkTitle);
     const linkRef = useRef(null);
-    const copyLink = (evt) => {
-        evt.preventDefault();
-        selectElementText(linkRef.current)
-        if ("clipboard" in navigator) {
-            navigator.clipboard.writeText(evt.target.href);
-            setTooltipText("Copied!")
-        }
+    const copiedLink = () => {
+        setTooltipText("Copied!");
     }
 
     const handleMouseLeave = () => {
@@ -44,9 +29,11 @@ export default function CopyLink({ children, className, title, ...props }) {
     return (
         <div className={styles.wrapper}>
             <span className={`${styles.tooltip} ${tooltipState ? styles.shown : styles.hidden}`} aria-hidden>{tooltipText}</span>
-            <NewTabLink title={linkTitle} onClick={copyLink} ref={linkRef} onMouseEnter={() => setTooltipState(true)} onMouseLeave={handleMouseLeave} className={`${styles.link} ${className}`} {...props}>
-                {children}
-            </NewTabLink>
+            <CopyToClipboard text={props.href} onCopy={copiedLink}>
+                <NewTabLink title={linkTitle} onClick={e => e.preventDefault()} onMouseEnter={() => setTooltipState(true)} onMouseLeave={handleMouseLeave} className={`${styles.link} ${className}`} {...props}>
+                    {children}
+                </NewTabLink>
+            </CopyToClipboard>
         </div>
     )
 }
