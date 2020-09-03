42756e6ad0be28f88cf4f4b6ebcd80a0bd597b62
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2020-09-04 08:07:14 +1000

    Add Header

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -8,7 +8,7 @@ import { themeStore } from "../stores";
  * Default layout component. 
  * Center content view with footer 
  */
-const Layout = ({ children }) => {
+function Layout(props) {
   let { theme/*, toggleTheme*/ } = themeStore
 
   let contentClass = "content";
@@ -16,10 +16,23 @@ const Layout = ({ children }) => {
   if (darkMode) {
     contentClass += " darkContent";
   }
+  if (props.noBackground) {
+    contentClass += " noBackground";
+  }
 
   return (
     <div className="layout">
-      <div className={contentClass}>{children}</div>
+      <header className="header">
+        <div className="pogifyLogo">
+          <img alt="Pogify Logo" className="pogifyLogoImage" src="/logo192.png"></img>
+          <p className="pogifyLogoText">POGIFY</p>
+        </div>
+        <div className="themeContainer">
+          <p className="themeText">Theme</p>
+          <button className="themeSwitch"></button>
+        </div>
+      </header>
+      <div className={contentClass}>{props.children}</div>
       <footer className="footer">
         <div className="footer-links">
           <Link to="/tou">Terms of Use</Link>
