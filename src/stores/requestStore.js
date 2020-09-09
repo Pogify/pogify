0cbae567d4bc9e5c57467bd6d8acb7f12d8fd90d
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-09-09 18:07:06 -0400

    more methods
    reqeustsstore now can subscribe, and unsubscribe to requests

--- src/stores/requestStore.js
+++ src/stores/requestStore.js
@@ -1,9 +1,38 @@
-import { extendObservable } from "mobx";
+import { extendObservable, action } from "mobx";
 
 export class RequestStore {
+  subscription = null;
   constructor() {
     extendObservable(this, {
-      requests: [],
+      requests: new Set(),
+      requestsOpen: false,
     });
   }
+
+  subscribeToRequests = (sessionId) => {
+    this.subscription = new EventSource(
+      "https://messages.pogify.net/requests/" + sessionId
+    );
+    this.subscription.onopen(() => {
+      this.requestsOpen = true;
+    });
+    this.subscription.onerror((e) => {
+      console.error(e);
+    });
+    this.subscription.onmessage(({ data }) => {
+      this.requests.add(data);
+    });
+  };
+
+  unsubscribeToRequests = action(() => {
+    if (this.subscription) {
+      this.subscription.close();
+      this.subscription = null;
+      this.requestsOpen = false;
+    }
+  });
+
+  removeRequest = (item) => {
+    this.requests.delete(item);
+  };
 }
