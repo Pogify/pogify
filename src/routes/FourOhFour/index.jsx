300f62bf5c26d780b1264e25f20c1585eccc778b
emtee14 <48140121+emtee14@users.noreply.github.com>
2020-11-10 21:13:56 +0000

    Fixes #26

--- src/routes/FourOhFour/index.jsx
+++ src/routes/FourOhFour/index.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 import { Layout } from "../../layouts";
 
-// import styles from "./index.module.css";
+import styles from "./index.module.css";
 
 /**
  * 404 component
@@ -21,9 +21,19 @@ export const FourOhFour = () => {
         </h1>
         <div>What you are looking for isn't here... What do you want to do?</div>
         <br></br><br></br>
-        <button onClick={() => window.history.back()} title="">Go back</button>
-        <br></br><br></br>
-        <Link to="/"><button>Go home</button></Link>
+        <center>
+          <div onClick={() => window.history.back()} title="">
+            <div className={styles.backButton}>
+              <div className={styles.backIcon}>
+                <img src="/back-arrow.svg" alt=""/>
+              </div>
+              <div className={styles.backText}>
+                Go Back
+              </div>
+            </div>
+          </div>
+          <br></br><br></br>
+        </center>
       </div>
     </Layout>
   );
